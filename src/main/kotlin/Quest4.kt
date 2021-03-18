0feffdbe62302728dc4376cd0fba8bc4d8744425
성민규 <123msn@naver.com>
2021-03-18 17:06:09 +0900

    quest4  업로드

---
+++ src/main/kotlin/Quest4.kt
@@ -0,0 +1,19 @@
+
+/*
+*
+* @author snow
+* @since 2021/03/18
+*/
+fun main(args :Array<String>) {
+	val quest4 = Quest4()
+	val answer = quest4.solution(1,1)
+}
+
+class Quest4 {
+	fun solution(w: Int, h: Int): Long {
+		var answer: Long = 0
+
+		return answer
+	}
+
+}
