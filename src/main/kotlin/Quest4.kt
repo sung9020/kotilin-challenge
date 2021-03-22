784672f71076bb0800146acf91cab938a7f83324
성민규 <123msn@naver.com>
2021-03-22 19:37:14 +0900

    누락된 문제 풀이 업로드

--- src/main/kotlin/Quest4.kt
+++ src/main/kotlin/Quest4.kt
@@ -6,14 +6,29 @@
 */
 fun main(args :Array<String>) {
 	val quest4 = Quest4()
-	val answer = quest4.solution(1,1)
+	val array: IntArray = intArrayOf(1, 5, 2, 6, 3, 7, 4)
+	val commands: Array<IntArray> = arrayOf( intArrayOf(2,5,3), intArrayOf(4, 4, 1), intArrayOf(1,7,3) )
+	val answer = quest4.solution(array,commands)
+	println(answer.joinToString(","))
 }
 
 class Quest4 {
-	fun solution(w: Int, h: Int): Long {
-		var answer: Long = 0
+	fun solution(array: IntArray, commands: Array<IntArray>): IntArray {
+		var answer = intArrayOf()
+		var answerList = arrayListOf<Int>()
 
-		return answer
+		for(command in commands){
+			val i = command[0]
+			val j = command[1]
+			val k = command[2]
+
+			var sliceArray = array.slice(IntRange(i-1,j-1))
+			sliceArray = sliceArray.sorted()
+			answerList.add(sliceArray[k-1])
+		}
+
+
+		return answerList.toIntArray()
 	}
 
 }
